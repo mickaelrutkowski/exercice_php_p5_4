1c3690a9f5175cfa8ec1f3500b2899e496de4548
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-27 15:52:28 +0100

    exercice_php_p5E4

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$month = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novenvbre', 'Décembre');
+echo 'nous somme au mois de' . ' ' . $month[7]= 'Août';
+?>
+<!-- jrfgikhgzergiurhgi -->
+ <?php $array = [
+    0 => 'janvier',
+     1 => 'fevrier',
+      2 => 'mars',
+       3 => 'avril',
+        4 => 'mai',
+         5 => 'juin',
+          6 => 'juillet',
+           7 => 'aout',
+            7 =>'août',
+             8 => 'septembre',
+              9 => 'octobre',
+               10 => 'novembre',
+                11 => 'decembre', ];
+                 echo $array[7]; ?>
